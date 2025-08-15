5a846683974b62d93005a01668b9128ab2f545e4
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2025-08-15 23:45:19 +0100

    update embedding

--- indexing.py
+++ indexing.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def prepare_indexed_chunks(folder_path, clean_func, chunk_func, embed_func):
     indexed_chunks = []
-    
+
 
     for filename in os.listdir(folder_path):
         if filename.endswith(".txt"):
@@ -16,11 +16,10 @@ def prepare_indexed_chunks(folder_path, clean_func, chunk_func, embed_func):
 
             # 2. Découper en chunks
             chunks = chunk_func(clean_text)
+            combined_texts = [f"{filename} {chunk}" for chunk in chunks]
+            embeddings = embed_func(combined_texts).detach().cpu().numpy().astype(np.float32)
 
-            # 3. Embedding + conversion directe en numpy float32
-            embeddings = embed_func(chunks).detach().cpu().numpy().astype(np.float32)
-
-            # 4. Normalisation
+            # 4. Normalisation: elle rend la comparaison des vecteurs plus fiable (cosine similarity)
             norms = np.linalg.norm(embeddings, axis=1, keepdims=True)
             embeddings = embeddings / norms
 
@@ -28,7 +27,7 @@ def prepare_indexed_chunks(folder_path, clean_func, chunk_func, embed_func):
             # 5. Stockage
             for i, (chunk, emb) in enumerate(zip(chunks, embeddings)):
                 indexed_chunks.append({
-                    "embedding": emb,  
+                    "embedding": emb,
                     "content": chunk,
                     "source": filename,
                     "chunk_index": i
