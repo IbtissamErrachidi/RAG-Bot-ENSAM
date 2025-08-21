c6b5bc250f74e51938ef2fa361510fce96233333
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2025-08-21 16:30:57 +0100

    add ConversationBufferMemory

--- rag.py
+++ rag.py
@@ -2,20 +2,48 @@ from cleaning import clean_text_keep_lines_and_paragraphs
 from chunking import chunk_text
 from vectorisation import embed
 import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+from langchain.memory import ConversationBufferMemory
+
+load_dotenv()
 
 
-api_key = userdata.get('GEMINI_API_KEY')
+api_key = os.getenv("GEMINI_API_KEY")
+
 genai.configure(api_key=api_key)
 
+
+
+memory = ConversationBufferMemory(
+    memory_key="chat_history",
+    return_messages=True
+)
+
+
+
+
 def rag_answer(query, retriever_func, index, indexed_chunks):
+
     context = retriever_func(query, index, indexed_chunks)
-    print(context)
+    print("Context:", context)
+
+    past_messages = memory.load_memory_variables({}).get("chat_history", [])
+
+    history_text = ""
+    for msg in past_messages:
+        role = "Utilisateur" if msg.type == "human" else "Assistant"
+        history_text += f"{role}: {msg.content}\n"
+
 
     prompt = f"""
 Vous êtes un assistant spécialisé dans l'ENSAM Casablanca.
 Répondez uniquement avec les informations présentes dans le contexte ci-dessous.
 Ne complétez jamais avec des informations inventées.
 Si l’information n’est pas dans le contexte, répondez "Je ne sais pas".
+
+Historique de conversation :
+{history_text}
 
 Contexte :
 {context}
@@ -30,4 +58,7 @@ Réponse :
     response = model.generate_content(prompt)
 
 
+    memory.save_context({"input": query}, {"output": response.text})
+
     return response.text
+
