ea5283082077ee81a1a36a8e8e1515346797e569
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2025-08-22 19:50:07 +0100

    load and chunk texts

---
+++ chunks.py
@@ -0,0 +1,28 @@
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from cleaning import clean_documents
+
+
+def load_and_chunk_texts(folder_path, chunk_size=800, chunk_overlap=100, separators=None):
+
+    loader = DirectoryLoader(
+        folder_path,
+        glob="*.txt",
+        loader_cls=lambda path: TextLoader(path, encoding="utf-8")  # <- encodage fixé
+    )
+    documents = loader.load()
+
+    if separators is None:
+        separators = ["\n\n","\n", "- ", "> ", ".", "!", "?", " ", ""]
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        separators=separators
+    )
+
+    documents = clean_documents(documents)
+
+    chunks = text_splitter.split_documents(documents)
+    return chunks
+
