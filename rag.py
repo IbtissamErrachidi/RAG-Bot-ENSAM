9bbe44a4e05d422c9052608118102c713f6c5d63
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2025-08-22 19:58:37 +0100

    build gemini conversational chain

---
+++ rag.py
@@ -0,0 +1,51 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import ChatPromptTemplate
+import os
+import google.generativeai as genai
+from config import GEMINI_API_KEY
+
+
+
+
+def build_gemini_conversational_chain(retriever, model_name="gemini-2.5-flash"):
+    """
+    Crée une chaîne RAG conversationnelle utilisant Gemini 2.5 Pro
+    avec un prompt spécialisé ENSAM Casablanca.
+    """
+
+    llm = ChatGoogleGenerativeAI(
+        model=model_name,
+        temperature=0,
+        api_key=os.environ.get("GEMINI_API_KEY") 
+    )
+
+    memory = ConversationBufferMemory(
+        memory_key="chat_history",
+        return_messages=True
+    )
+
+
+    custom_prompt = ChatPromptTemplate.from_template("""
+    Vous êtes un assistant spécialisé dans l'ENSAM Casablanca.
+    Si l'utilisateur envoie une salutation ou un message amical (ex : "hi", "hello", "bonjour", "salut", "hey", "coucou"), répondez par un message d'accueil amical et standard :
+    "Bonjour ! Je suis l'assistant de l'ENSAM Casablanca, comment puis-je vous aider ?"
+    Répondez uniquement avec les informations présentes dans le contexte ci-dessous.
+    Ne complétez jamais avec des informations inventées.
+    Informez toujours l'utilisateur lorsque vous n'avez pas trouvé la réponse gentiment - soyez honnête.
+
+    Contexte : {context}
+    Historique de la conversation : {chat_history}
+    Question : {question}
+    Réponse :
+    """)
+
+    conv_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=retriever,
+        memory=memory,
+        combine_docs_chain_kwargs={"prompt": custom_prompt}
+    )
+
+    return conv_chain
