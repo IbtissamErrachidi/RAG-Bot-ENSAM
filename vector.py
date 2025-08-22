42256fb2695b7c2ebdb0499ea0fe9f9b5824302d
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2025-08-22 19:55:33 +0100

    build vectordb

---
+++ vector.py
@@ -0,0 +1,20 @@
+from langchain.vectorstores import FAISS
+from langchain.embeddings import HuggingFaceEmbeddings
+import os
+
+
+def build_vectordb(chunks, model_name="all-MiniLM-L6-v2"):
+    """
+    chunks: liste de Document
+    model_name: nom du modèle pour embeddings
+    """
+    embeddings = HuggingFaceEmbeddings(model_name=model_name)
+    vectordb = FAISS.from_documents(chunks, embeddings) 
+ 
+    save_path = "vectordb_local"
+    os.makedirs(save_path, exist_ok=True)
+    vectordb.save_local(save_path)
+    print(f"Vectordb sauvegardé localement dans : {save_path}")
+
+    return vectordb
+
