43f5c3a2c6be0154d5d604c5ee6b60ebc629a399
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2025-08-28 21:35:11 +0100

    add crawling logic

--- streamlit.py
+++ streamlit.py
@@ -3,10 +3,17 @@ from rag import build_gemini_conversational_chain
 from vector import build_vectordb
 import streamlit as st
 from langchain.schema import Document
-from langchain.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from chunks import load_and_chunk_texts
 import os
+import threading
+import time
+import schedule
+from update_corpus import update_corpus1
+import threading
+
+vectordb_lock = threading.Lock()
 
 
 st.set_page_config(page_title="💬 Chat ENSAM", page_icon="🎓", layout="wide")
@@ -48,10 +55,12 @@ def send_message():
     query = st.session_state.user_input
     if query:
         with st.spinner("💬 Chat en cours..."):
-            response = st.session_state.chain.run(query)
+            with vectordb_lock:
+                response = st.session_state.chain.run(query)
            
             st.session_state.chat_history.append((query, response))
-        st.session_state.user_input = ""  
+        st.session_state.user_input = ""
+
 
 if st.session_state.vectordb_ready:
     st.success("✅ Chat prêt ! Vous pouvez poser votre question ci-dessous.")
@@ -65,13 +74,60 @@ if st.session_state.vectordb_ready:
         st.markdown("---")
 
     st.text_input(
-        label="",  
-        placeholder="Posez votre question ici :",  
-        key="user_input",
-        on_change=send_message
-    )
+    label="Question :", 
+    placeholder="Posez votre question ici :",  
+    key="user_input",
+    on_change=send_message,
+    label_visibility="hidden" 
+)
+
 
 
 
     if st.button("Envoyer"):
         send_message()
+
+
+
+def load_vectordb_to_session_thread():
+    print("🔄 Préparation du chat en arrière-plan...")
+    
+
+    new_vectordb = FAISS.load_local(
+        vectordb_path,
+        HuggingFaceEmbeddings(model_name=embedding_model_name),
+        allow_dangerous_deserialization=True
+    )
+    new_retriever = get_retriever(new_vectordb)
+    new_chain = build_gemini_conversational_chain(new_retriever)
+    
+
+    with vectordb_lock:
+        st.session_state.vectordb = new_vectordb
+        st.session_state.retriever = new_retriever
+        st.session_state.chain = new_chain
+        st.session_state.vectordb_ready = True
+
+    print("✅ Chat prêt avec la nouvelle version du vectordb !")
+
+# -------------------- Background update --------------------
+def scheduled_update():
+    st.session_state.vectordb_ready = False
+    
+    update_corpus1()  
+    load_vectordb_to_session_thread()  
+    
+
+
+def run_schedule():
+    schedule.every(45).minutes.do(scheduled_update)
+    while True:
+        try:
+            schedule.run_pending()
+        except Exception as e:
+            print(f"Erreur dans schedule : {e}")
+        time.sleep(30)
+
+
+threading.Thread(target=run_schedule, daemon=True).start()
+
